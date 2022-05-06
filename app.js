82445109a48f4a0538ca02a7eacfe3263c916141
Armen-developer <104728413+Armen-developer@users.noreply.github.com>
2022-05-06 18:35:16 +0300

    1

---
+++ app.js
@@ -0,0 +1,8 @@
+const name = 'Armen'
+const lastName = 'Gulyan'
+
+//const guestName = prompt('Напишите свое имя')
+//const guestLastName = prompt('Напишите свою Фамилию')
+//const fullName = guestName + ' ' + guestLastName
+
+console.log (fullName)
